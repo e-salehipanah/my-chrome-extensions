3100b7dcb7bf8e4b94284cf957ba70a20d9aaae1
Ebrahim Salehipanah <e.salehipanah@gmail.com>
2022-03-31 13:59:09 +0430

    pane

--- devtools/devtools.js
+++ devtools/devtools.js
@@ -1,6 +1,19 @@
- chrome.devtools.panels.create(
-     'Example panel',
-     null,
-     'panel/panel.html',
-     null 
- )
+chrome.devtools.panels.create("Example panel", null, "panel/panel.html", null);
+
+// chrome.devtools.panels.elements.createSidebarPane('My sidebar pane');
+chrome.devtools.panels.sources.createSidebarPane(
+  "My sidebar pane",
+  function (pane) {
+    pane.setPage("pane/pane.html");
+    pane.setHeight("200px");
+    pane.onShown.addListener(function () {
+      pane.setExpression('('+getAllDives.toString()+')()')
+    });
+  }
+);
+
+function getAllDives() {
+    let divs = Array.prototype.slice.call(document.getElementsByTagName('div'));
+    divs.__proto__ = null;
+    return divs;
+}
