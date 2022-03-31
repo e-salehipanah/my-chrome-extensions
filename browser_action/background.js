2fada0eea85bf423a918ae563167957a9d5db4eb
Ebrahim Salehipanah <e.salehipanah@gmail.com>
2022-03-31 16:01:02 +0430

    comunicate between content script & bg/event page

--- browser_action/background.js
+++ browser_action/background.js
@@ -1,8 +1,23 @@
-function handleBackground(){
-    chrome.tabs.create({
-        index : 0,
-        url : 'http://google.co.uk'
-    },function(tab){
-        console.log(tab);
-    })
+function handleBackground() {
+  chrome.tabs.create(
+    {
+      index: 0,
+      url: "http://google.co.uk",
+    },
+    function (tab) {
+      console.log(tab);
+    }
+  );
 }
+
+chrome.runtime.onMessage.addListener(function (message, sender) {
+  console.log(message, sender);
+  response({ status: "success!" });
+});
+
+chrome.runtime.onConnect.addListener(function (messagePort) {
+  messagePort.onMessage.addListener(function (message) {
+    console.log(message.h2);
+    messagePort.postMessage({ status: "received!" });
+  });
+});
--- browser_action/content.js
+++ browser_action/content.js
@@ -1,4 +1,27 @@
-(function(){
-    'use strict';
-    console.log("hello world!");
-}())
+(function () {
+  "use strict";
+  console.log("hello world!");
+
+  var h1 = document.getElementsByClassName("h1")[0];
+  chrome.runtime.sendMessage(
+    {
+      firstHeading: h1.textContent,
+    },
+    function (response) {
+      console.log(response.status);
+    }
+  );
+
+  var messagePort = chrome.runtime.connect({
+    name: "heading",
+  });
+
+  var h2s = Array.slice.call(document.getElementsBYTagName("h2"));
+  h2s.forEach(function (h2){
+      messagePort.postMessage({h2:h2.textContent})
+  })
+
+  messagePort.onMessage.addListener(function (message){
+      console.log(message.status);
+  })
+})();
